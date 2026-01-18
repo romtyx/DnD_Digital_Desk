316614388198aab888871be90826b40fca4ac3d3
Televisor12345 <voevodin_timofey09@mail.ru>
2026-01-18 19:39:22 +0300

    add registration

---
+++ backend/accounts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import RegisterForm
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('admin:index')
+    else:
+        form = RegisterForm()
+    return render(request, 'registration/register.html', {'form': form})
